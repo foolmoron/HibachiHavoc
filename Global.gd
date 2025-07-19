4a9a7c1a6f1782a5a983ebe2844e7b93d801a2ea
Rachel <rachelsgcount@gmail.com>
2025-07-19 13:44:53 -0400

    trying to get globals working

--- Global.gd
+++ Global.gd
@@ -1,10 +1,57 @@
 extends Node
 
-var audioControl : AudioControl
+var currentScene : String
+var level1 : PackedScene = preload("res://scenes/level1.tscn")
+var level2 : PackedScene = preload("res://scenes/level2.tscn")
+var level3 : PackedScene = preload("res://scenes/level3.tscn")
 
-func reset():
-	audioControl.switchMusic()
+#variables to return to titlescreen after no user interaction for a while
+var idleTimer : Timer
+@export var idle_delay : int = 60
 
-func switchScene(nextScene : PackedScene):
-	get_tree().change_scene_to_packed(nextScene)
-	
+func _ready() -> void:
+	idleTimer = Timer.new()
+	idleTimer.wait_time = idle_delay   #wait for idleness
+	currentScene
+	switchMusic("menu")
+	_loadScene(level1)
+
+#switch to next scene and change music to that scene's music; 
+func switchScene(nextScene : String):
+	while nextScene != "":
+		match nextScene:
+			"idleOverlay":
+				nextScene = currentScene
+				break
+			"level1":
+				_loadScene(level1)
+			"level2":
+				_loadScene(level2)
+			"level3":
+				_loadScene(level3)
+		switchMusic("menu")
+		nextScene = ""
+
+func _loadScene(scene : PackedScene):
+	get_tree().change_scene_to_packed(scene)
+
+
+### AUDIO CONTROL ###
+func switchMusic(song : String):
+	AudioController.stream = load(song)
+	AudioController.play()
+
+func eat():
+	var eatSFX = AudioController.get_child(0)
+	if(eatSFX.playing):
+		eatSFX.stop()
+	eatSFX.play()
+
+
+### IDLE CONTROL ###
+func _on_IdleTimer_Timeout():
+	switchScene("idleOverlay")
+
+func _physics_process(delta: float) -> void:
+	if (false):    # if input happens at any time, reset the idleTimer. 
+		idleTimer.start()
--- scenes/lose_transition.gd
+++ scenes/lose_transition.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var delay_in_sec := 5.0 
 @onready var timer : Timer = $Timer
-var countdown_base := "Back to title in "
+@onready var countdown := $CanvasLayer/VBoxContainer/Countdown
 
 func _ready() -> void:
-	pass
+	timer.wait_time = delay_in_sec
 
+func _physics_process(_delta: float) -> void:
+	countdown.text = str(timer.time_left).pad_decimals(1)
 
 func _on_timer_timeout() -> void:
-	Global.switchScene()
+	Global.switchScene("idleOverlay")
