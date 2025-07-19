d6892b7f3909faf22d9a41ae2db9cff360646624
Rachel <rachelsgcount@gmail.com>
2025-07-19 18:52:33 -0400

    help plz

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,15 @@
 extends Area2D
 
-var isTouchingFood : bool
+@onready var isTouchingFood : bool
 
-func _on_body_entered(body: Node2D):
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("food"):
+		isTouchingFood = true
 
 func _on_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+
+#when mouth close
+#check if mouth open
+#check if food there
+#close mouth bool
