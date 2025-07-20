c9beba77facefb194a44757ff5fada3e91666fae
Momin Khan <foolmoron@gmail.com>
2025-07-20 15:57:14 -0400

    stuff

--- scenes/idle_overlay.gd
+++ scenes/idle_overlay.gd
@@ -5,15 +5,12 @@ signal startGame()
 @export var foodsToStart: Array[Node] = []
 
 func _ready() -> void:
+	$CanvasLayer.show()
 	for food in foodsToStart:
 		food.tree_exited.connect(_on_Food_Ate.bind(food))
 
 func _on_Food_Ate(node: Node):
 	foodsToStart.erase(node)
 	if foodsToStart.size() == 0:
+		$CanvasLayer.hide()
 		startGame.emit()
-
-#TEMPORARY
-func _input(event: InputEvent) -> void:
-	if event.is_action("TempInput"):
-		_on_Food_Ate(foodsToStart[0])
