9c5cac13c49d0293817018bf90bea4410db8cc02
Momin Khan <foolmoron@gmail.com>
2025-07-20 17:42:53 -0400

    idle reload

--- main.gd
+++ main.gd
@@ -2,4 +2,4 @@ extends Node
 
 # Load 1st level when loading from main scene
 func _ready() -> void:
-	Global._loadScene.bind(Global.levels[0]).call_deferred()
+	Global.switchScene.bind(0).call_deferred()
